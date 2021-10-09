abc593a87eff30f209eef6c990cf305476bf8e37
hiremasa <syorikunn0901@gmail.com>
2021-10-09 22:50:33 +0900

    section 80

--- chapter08/postmortem_breakpoint.py
+++ chapter08/postmortem_breakpoint.py
@@ -1,4 +1,5 @@
-# https://www.youtube.com/watch?v=aZJnGOwzHtU
+import math
+
 def compute_rmse(observed, ideal):
     total_err_2 = 0
     count = 0
@@ -10,8 +11,9 @@ def compute_rmse(observed, ideal):
         count += 1
 
     mean_err = total_err_2 / count
-    rmse = math.sqrt(mean_err)
+    rmse = marth.sqrt(mean_err)
     return rmse
 
-result = compute_rmse([1.8, 1.7, 3.2, 7], [2, 1.5, 3, 5])
+result = compute_rmse([1.8, 1.7, 3.2, 7j])
 print(result)
+
